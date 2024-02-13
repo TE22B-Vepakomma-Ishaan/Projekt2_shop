0b9cceb5cf8d5930aa4034bb3f1c51559a0c949d
Ishaan Vepakomma <ishaan.vepakomma@elev.ga.ntig.se>
2024-02-13 14:56:14 +0100

    commit3- 13/2

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+function openMenu() {
+    var x = document.getElementById("product_item_links");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  } 
+
+
+  
