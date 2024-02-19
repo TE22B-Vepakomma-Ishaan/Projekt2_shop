1bbdc7d557f9c97acb3e9b0f4ed88e50fab45d5f
Ishaan Vepakomma <ishaan.vepakomma@elev.ga.ntig.se>
2024-02-19 09:18:06 +0100

    19/2

--- script.js
+++ script.js
@@ -2,11 +2,18 @@
 
 function openMenu() {
     var x = document.getElementById("product_item_links");
+    
     if (x.style.display === "block") {
       x.style.display = "none";
     } else {
       x.style.display = "block";
+      x.style.filter = "greyscale(60%)";
     }
+
+    let prom =document.querySelector("#promoted_page");
+    prom.style.backgroundColor="grey";
+    // document.body.style.filter="grayscale(100%)";
+
   } 
 
 
